e58f56c0445802262eafb7bc73b7565c40c35ad9
seonyeonghun <seonyeonghun@kakao.com>
2025-02-11 16:53:57 +0900

    검색 유무에 따른 데이터 fetch

---
+++ frontend/read.js
@@ -0,0 +1,50 @@
+// 검색어 없이, 조회 버튼 누를때,
+const read_form = document.querySelector("#read_form");
+const list_ul = document.querySelector("#contacts_list");
+const search = document.querySelector("#keyword");
+let items = "";
+const server_url = "http://localhost:3000/contacts";
+read_form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if(search.value !== "") {
+    logJSONData(`${server_url}?name=${search.value}`);
+  } else {
+    logJSONData(server_url);
+  }
+});
+
+async function logJSONData(url) {
+  const response = await fetch(url);
+  const jsonData = await response.json();
+//   console.log(jsonData);
+  for (let data of jsonData) {
+    items += `
+        <li class="box d-flex align-center gap-1">
+            <figure class="photo flex-1">
+                <img src="images/${data.gender}.png" width="100" alt="${data.gender}">
+                <figcaption>${data.name}</figcaption>
+            </figure>
+            <ul class="info flex-2">
+                <li>이름 : ${data.name}</li>
+                <li>전화번호 : ${data.mobile}</li>
+                <li>메모: ${data.memo}</li>
+                <li>하는일 : ${data.job}</li>
+            </ul>
+            <div class="btns flex-2 text-center">
+                <button type="button" class="btn edit"><img src="images/edit.png" width="24" alt="수정"></button>
+                <button type="button" class="btn remove"><img src="images/remove.png" width="24" alt="삭제"></button>
+                <button type="button" class="btn sms"><img src="images/sms.png" width="24" alt="문자전송"></button>
+                <button type="button" class="btn call"><img src="images/call.png" width="24" alt="전화걸기"></button>
+            </div>
+        </li>
+        `;
+  }
+  list_ul.innerHTML = items;
+}
+
+search.addEventListener("change", function(){
+    items = "";
+    list_ul.innerHTML = items;
+})
+
+// 검색어 넣고, 조회버튼 누를때
