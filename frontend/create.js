935e61db4582c8a0bd49d140d1ee3f3019537aad
seonyeonghun <seonyeonghun@kakao.com>
2025-02-12 10:17:46 +0900

    연락처 등록 | 폼 입력값 체크 스크립트 수정, 등록요청시 전달할 데이터를 객체화

--- frontend/create.js
+++ frontend/create.js
@@ -1,36 +1,47 @@
 const createForm = document.querySelector("#create_form");
 const server_url = "http://localhost:3000/contacts";
-createForm.addEventListener("submit", async function(e){
+createForm.addEventListener("submit", function(e){
     e.preventDefault();
 
     const name = createForm.querySelector("#name");
-    const mobile = createForm.querySelector("#mobile").value;
-    const memo = createForm.querySelector("#memo").value;
-    const gender = createForm.querySelector("#gender").value;
-    const job = createForm.querySelector("#job").value;
+    const mobile = createForm.querySelector("#mobile");
+    const memo = createForm.querySelector("#memo");
+    const gender = createForm.querySelector("#gender");
+    const job = createForm.querySelector("#job");
 
     if (name.value == "") {
-        alert("입력하세요!");
+        alert("이름을 입력하세요!");
         name.focus();
+        return;
     } else if (mobile.value == "") {
+        alert("전화번호를 입력하세요!");
         mobile.focus();
+        return;
+    } else if (memo.value == "") {
+        alert("메모를 입력하세요!");
+        memo.focus();
+        return;
+    } else if (gender.value == "") {
+        alert("성별을 선택하세요!");
+        gender.focus();
+        return;
+    } else if (job.value == "") {
+        alert("직업을 입력하세요!");
+        job.focus();
+        return;
     }
     
     let inputData = {
-        name,
-        mobile,
-        memo,
-        gender,
-        job
+        name: name.value,
+        mobile: mobile.value,
+        memo: memo.value,
+        gender: gender.value,
+        job: job.value
     }    
-    console.log(inputData);
 
-    const result = await postData(server_url, inputData);
-    console.log(result);
+    //console.log(inputData);
 
-    // postData(server_url, inputData).then(data => {
-    //     console.log(data);
-    // })    
+    postData(server_url, inputData); // 등록 요청 함수를 호출 (데이터와 서버주소 전달)
 })
 
 async function postData(url="", data={}){
